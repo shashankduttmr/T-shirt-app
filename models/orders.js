eba734a0c65577fe6f0a17ba3bf2a6ee079cd199
shashankduttmr <shashankduttm@gmail.com>
2023-03-21 01:58:09 +0530

    Order Informations

--- models/orders.js
+++ models/orders.js
@@ -3,23 +3,66 @@ const mongoose = require('mongoose')
 const {Schema} = mongoose
 
 const MyOrders = new Schema({
-    shippingAddress:{
-        type:String,
-        required:[true]
+    shippingInfo:{
+        address:{
+            type:String,
+            required:true
+        },
+        PhoneNumber:{
+            type:String,
+            required:true
+        },
+        city:{
+            type:String,
+            required:true
+        },
+        state:{
+            type:String,
+            required:true
+        },
+        postalcode:{
+            type:Number,
+            required:true
+        },
+        country:{
+            type:String,
+            required:true
+        },
     },
-    
+
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:'user',
+        required:true
+    },
+
+    orderItem:[
+        {
+            name:{
+                type:String,
+                required:true
+            },
+            quantity:{
+                type:Number,
+                required:true
+            },
+            price:{
+                type:Number,
+                required:true //quantity*price
+            },
+            product:{
+                type:Schema.Types.ObjectId,
+                ref:'Product',
+                required:true
+            }
+        }
+    ],
+    paymentInfo:{
+        id:{
+            type:String
+        }
+    }
 })
-
-
-
-
-
-
-
-
-
-
-
 
 
 // Shipping Information {},
