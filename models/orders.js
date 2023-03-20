429150c56cd97b725af9113661285b9baf65aa28
shashankduttmr <shashankduttm@gmail.com>
2023-03-20 14:10:10 +0530

    order models

--- models/orders.js
+++ models/orders.js
@@ -3,5 +3,37 @@ const mongoose = require('mongoose')
 const {Schema} = mongoose
 
 const MyOrders = new Schema({
+    shippingaddress:{
+        type:String,
+        required:[true]
+    },
     
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Shipping Information {},
+// User,
+// Payment's info{},
+// Tax Amount,
+// Shipping Amount,
+// Total Amount,
+// Order Status
+// Created At
+// ------------------------
+// orderItem:[{}]
+// - name
+// quantity
+// Image[0]
+// price
+// Product
