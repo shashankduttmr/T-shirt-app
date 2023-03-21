87ee30e8a5303e9cfad5e47719f2bb2c4fec6c6c
shashankduttmr <shashankduttm@gmail.com>
2023-03-21 13:23:02 +0530

    Updated route

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Router = express.Router()
 const {createOrder} = require('../controllers/Orders')
+const {isLoggedin} = require('../middlewares/isloggedin')
 
 
 
